7aa83daeb29614b04a986fd62ee04f75f71f3bcf
Azamat Mur <Azamattsunami@mail.com>
2020-06-29 12:00:07 -0400

    reactions updated

--- reactions.py
+++ reactions.py
@@ -2,24 +2,28 @@ from string import ascii_letters
 
 from substance import Substance
 
+from itertools import product
+import numpy as np
+
 
 class Reaction:
     def __init__(self, reaction):
         self.plain_reaction = reaction
         self.sanitize()
-        self.split()
+        self.sepparate()
         self.find_atoms()
         self.solve()
 
     def sanitize(self):
         # Delete all unecessary chars
         chars_to_left = ascii_letters + '0123456789()->=+'
-        self.clean_form = []
+        temp = []
         for char in self.plain_reaction:
-            if char in char_to_left:
-                self.clean_form += char
+            if char in chars_to_left:
+                temp += char
+        self.clean_form = ''.join(temp)
 
-    def split(self):
+    def sepparate(self):
         self.reactants = []
         self.products = []
 
@@ -44,25 +48,36 @@ class Reaction:
         self.products = [Substance(item) for item in self.products]
 
         for subs in self.reactants:
-            self.atoms.add(subs.composition.keys())
-
-        self.reactants = [Substance('H2'), Substance('O2')]
-        self.products = [Substance('H2O')]
+            self.atoms.update(set(subs.composition.keys()))
 
     def solve(self):
-    'def matrices(self):'
+        coeffs = np.array([[1, 0, -2, 0, 0, 0], [1, 0, 0, 0, -1, 0],
+                           [3, 4, -12, 0, -2, -1], [0, 2, 0, 0, 0, -2], [0, 1, -3, -1, 0, 0]])
+
+        combs = product(range(1, 12), repeat=6)
+
+        for i in combs:
+            solution = np.array(i)
+            res = coeffs.dot(solution)
+
+            if np.count_nonzero(res, axis=None) == 0:
+                print(res, solution)
+                break
 
     def __repr__(self):
         solution = ''
 
-        for item in self.reactants:
-            pass
+        solution += ' + '.join([item.pretty_formula for item in self.reactants])
 
-        solution += '->'
+        solution += ' -> '
 
-        for item in self.products:
-            pass
+        solution += ' + '.join([item.pretty_formula for item in self.products])
 
         return f'''Reaction given: {self.plain_reaction}
         Solution: {solution}
         '''
+
+
+if __name__ == "__main__":
+    test = Reaction('H2 + O2 = H2O')
+    print(test)
