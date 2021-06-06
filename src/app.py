f29fa53aa1de036b7bdb632cd14883d0a8a02ce2
Azamat Mur <Azamattsunami@mail.com>
2021-06-06 12:42:28 -0400

    app.py changes

--- src/app.py
+++ src/app.py
@@ -2,11 +2,14 @@
 Main entry point
 '''
 
+from core.reaction import Reaction
+
 from ui.tkinter.main import start_gui
 
 
 if __name__ == '__main__':
     start_gui()
+    #react = Reaction('FeCl3 + NaOH = Fe(OH)3 + NaCl')
     # if ui.args.args['gui']:
     #     ui.gui.run()
     # else:
