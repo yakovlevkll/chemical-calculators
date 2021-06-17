95d9becf7ebe07406fe559ac66f8774a1940c055
Azamat Mur <Azamattsunami@mail.com>
2021-06-17 12:30:02 -0400

    Cleaned and finalized unit parsing

--- src/core/quantities.py
+++ src/core/quantities.py
@@ -1,7 +1,7 @@
 
 from .units import MassUnit, VolumeUnit, Moles, Unit
 import re
-from typing import Optional
+from typing import Optional, Tuple
 
 # cases
 # 1. given quantities -> find excess or how much is needed
@@ -17,17 +17,21 @@ class Quantities:
     def __init__(self, unit_quantity: str, molar_mass: float):
         self.user_input = unit_quantity
         # finds excess
-        coeff, unit_str = self.find_coeff_unit_pair()
+        coeff, unit_str = self.split_coeff()
+        
         self.coeff = coeff
+        
         self.unit = self.check_unit(unit_str)
-
-        self.excess()
         # converts everything to moles
         # self.to_moles()
+        self.quantity_value = coeff * 10**self.unit.final_exponent * self.unit.conv_value
+        
 
-    def find_coeff_unit_pair(self):
+    def split_coeff(self) -> Tuple[float, str]:
         '''
-        Returns (5, 'kg') from 5kg
+        separates coefficient from the index-unit
+        Example 1: 5kg --> 5, kg
+        Example 2: 4cl --> 4, cl
         '''
         # combines base units into a string
         
@@ -36,60 +40,54 @@ class Quantities:
         # Find with regexp
         match = re.match(regexp, self.user_input)
         if match:
-            return float(match.group(1)), match.group(2)
+            coeff = float(match.group(1)) if match.group(1) else 1
+            return coeff, match.group(2)
         else:
             raise ValueError
     
     def get_SI_coeff(self):
-        return self.coeff * self.unit.
+        print(self.coeff*self.unit.conv_value)
+        return self.coeff * self.unit.conv_value
         
-    def check_unit(self, unit_str: str) -> Unit:
+    def check_unit(self, unit_str: str) -> Unit: #Some kind of error here
+        '''
+        Checks to determine the appropriate class (i.e. Mass, Volume or Moles)
+
+        Runs units.py through each class until a non-error solution is obtained
+        '''
         unit_classes = [MassUnit, VolumeUnit, Moles]
-        res = None
+
         for unit_cls in unit_classes:
             try:
                 return unit_cls(unit_str)
-            finally:
-                raise ValueError(f'Unknown units given - {unit_str}')
+            except:
+                continue
 
-    def excess(self):
-        '''
-        difference between mass on right and mass on left
-        '''
-        pass
+        raise ValueError(f'Unknown units given - {unit_str}')
 
-    def to_moles(self, amount: float, units: str):
+    def get_moles(self) -> float:
         '''
-        Generic converts units to moles
-        Calls situation-specific functions
+        Get quantity in moles
         '''
-        pass
-
-    def mass_to_aos(self) -> float:
-        '''
-        converts mass (xg) to amount of substance (moles)
-        '''
-
+        if isinstance(self.unit, MassUnit):
+            pass
+        elif isinstance(self.unit, VolumeUnit):
+            pass
 
         return 0.0
 
-    def volume_to_aos(self) -> float:
+    def get_mass(self, unit: str) -> float:
+        '''
+        get quantity in specified mass units
+        '''
+
+        return 0.0
+
+    def get_volume(self, unit: str) -> float:
         '''
         converts volume (xl, xm^3) to amount of substance (moles)
         '''
         return 0.0
-
-    def aos_to_mass(self, units: MassUnit):
-        '''
-        converts amount of substance (moles) to mass (g, kg, ...)
-        '''
-        pass
-
-    def aos_to_volume(self, units: VolumeUnit):
-        '''
-        converts amount of substance (moles) to volume (ml, l, m3, dm3, cm3, ...)
-        '''
-        pass
 
 
 
--- src/core/units.py
+++ src/core/units.py
@@ -1,45 +1,80 @@
 import re
-from typing import NamedTuple
+from typing import NamedTuple, Tuple
+
+
+class FactorExpPair(NamedTuple):
+    '''
+    Creates parameters for  - Unit - Conversion - Exponent - relations
+    Example 1: (gram, 1 (1 g = 1 g), 1 (exponent 1 applied to prefix)) --> (gram, 1, 1)
+    Example 2: (pound, 453.592 (453.592 g = 1 lb), 1 (exponent 1 applied to prefix)) --> (pound, 453.592, 1)
+    Example 3: (cubic inch, 0.0254^3 (0.0254^3 m^3 = 1 in^3), 3 (exponent 3 applied to prefix) --> (cubic inch, 0.0254^3, 3)
+    '''
+    names: list[str]
+    # Hom nany units in base units, e.g. hom many meters in inch
+    conv_value: float
+    exp: float
+   
 
 
 class Unit:
-    user_unit: str
-    power: int = 0
-
+    # holds user data (example cm^3, g, inch^3, etc.)
+    user_input: str
+    # conversion value, how many base units (g or m^3) fit into another unit (example: pound -> grams)
+    conv_value: float
+    # the final exponent with base 10. Is a multiplication of prefix exponent and unit exponent
+    final_exponent: float
+    
     prefixes = {
         'm': -3,
         'c': -2,
-        'k':3,
-        'M':6,
+        'k': 3,
+        'M': 6,
     }
 
-    # Must be defined in children classes
-    BASE_UNIT: str
-    BASE_POWER: int
-    CONVERSIONS: dict[str, float]
+    # Must be defined in children classes                                                       #cm^3
+    DATA: list[FactorExpPair]
 
     def __init__(self, unit: str):
         '''
-        unit: `kg`, for example
+        unit: `cm^3`, for example
         '''
-        if not (self.CONVERSIONS and self.BASE_UNIT and self.BASE_POWER):
+        if not (self.DATA):
             raise NotImplementedError('Child unit class must provide ...')
         
-        prefix, unit = self.split_into_components(unit)
+        prefix, unit = self.split_into_components(unit)                                         # c, m^3
 
-        self.user_unit = unit
+        self.user_input = unit                                                                   # m^3
 
+        data = self.get_data()                                                                  #(m^3, 1, 3)
+
+      
+        # Applies the appropriate exponent in accordance with the prefix
         if prefix:
-            self.power = self.prefixes[prefix]
+            self.final_exponent = self.prefixes[prefix] * data.exp                             # -2 * 3 = -6
+        else:
+            self.final_exponent = data.exp
 
-    def split_into_components(self, unit):
+    def get_data(self) -> FactorExpPair:
         '''
-        Returns ('k', 'g') from kg
+        Checks what unit it is (g, in, l, foot, etc.) in one of the child classes (depending on the iteration in check_unit func inside .quantities)
+    
+        Returns FactorExpPair
+        '''
+        for el in self.DATA:                      
+            if self.user_input in el.names: 
+                return el                                                                       #(m^3, 1, 3)
+        raise ValueError()
+
+    def split_into_components(self, unit) -> Tuple[str, str]:
+        '''
+        Splits the prefix from the units
+        Example 1: kg -> k, g
+        Example 2: cl -> c, l
         '''
 
         # combines base units into a string
         prefix = '|'.join(self.prefixes.keys())
-        units = '|'.join(self.CONVERSIONS.keys())# --> 'g'
+        units = '|'.join(self.naming_list)# --> 'g'
         regexp = f'^({prefix})*({units})$'
         # regexp = f'^(\d+\.{0,1}\d*)(m|k|M)*(g|pound|l)$'
         # Find with regexp
@@ -49,35 +84,65 @@ class Unit:
         else:
             raise ValueError
 
+
     def convert(self, to: str):
         # TODO: Implement
         pass
 
     def convert_to_user_unit(self):
-        return self.convert(self.user_unit)
+        return self.convert(self.user_input)
 
-class FactorExpPair(NamedTuple):
-    factor: float
-    exp: float
+    @property
+    def naming_list(self):
+        '''
+        Creates a list of all possible units within MassUnit or VolumeUnit class (whichever is appropriate)
+        This allows the program to check if the user has entered a recognized unit 
+        (i.e. g, gram, pound, l, m^3 etc. and not something random)
+        '''
+        res = []
+        for el in self.DATA:
+            res += el.names
+        return res
+
+        
+        
 
 
 class MassUnit(Unit):
-    BASE_UNITS = ['g']
-    CONVERSIONS = {
-        'g': 1,
-        'ounce': 2.424,
-    }
-    BASE_POWER = 1
+    '''
+    Deals with mass units
+    Base unit: Gram
+    Examples: Gram, Pound, Short Tonne, Ounce, etc.
+    '''
+    DATA = [
+        # Base unit
+        FactorExpPair(['g','gram', 'grams'], 1, 1),
+        # Other units
+        FactorExpPair(['oz','ounce','ounces'], 28.3495, 1)
+    ]
     
+# Empirical is for unit-austronauts, metric is for real humans
+# Conversions below (MOVE TO A DIFFERENT FILE MAYBE?)
+INCH = 0.0254
 
 class VolumeUnit(Unit):
-    BASE_UNITS = ['l', 'm^3']
-    CONVERSIONS = {
-        'm^3': FactorExpPair(1, 3),
-        'l': FactorExpPair(0.001, 1),
-    }
-    BASE_POWER = 3
+    '''
+    Deals with volume units
+    Base unit: Cubic meter
+    Examples: Cubic meter, Cubic inch, Cubic foot, etc.
+    '''
+    DATA = [
+        # Base unit
+        FactorExpPair(['m^3', 'm3'], 1, 3),
+        # Other units
+        FactorExpPair(['l','litre','litres'], 0.001, 1),
+        FactorExpPair(['inch^3', 'inch3'], INCH ** 3, 3),
+    ]
 
 
 class Moles(Unit):
-    pass
+    DATA = [
+        # Base unit
+        FactorExpPair(['mol', 'moles'], 1, 1),
+        
+    ]
