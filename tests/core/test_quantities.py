1160e38e1c7ae537eaa74e670ddcd83eb29e512f
Azamat Mur <Azamattsunami@mail.com>
2021-06-27 12:43:14 -0400

    quantity + unit tests update

--- tests/core/test_quantities.py
+++ tests/core/test_quantities.py
@@ -2,21 +2,28 @@ import pytest
 from core.quantities import Quantities
 
 def test_self_mass():
-    q = Quantities('2.5kg', 1)
+    q = Quantities('2.5kg', 3, 'Mass')
 
     assert q.to_mass() == 2500
 
 def test_self_volume():
-    q = Quantities('1500cl', 1)
+    q = Quantities('1500cl', 1, 'Volume')
 
     assert q.to_volume() == 0.015
 
 def test_self_moles():
-    q = Quantities('30mol', 1)
+    q = Quantities('30mol', 1, 'Mole')
 
     assert q.to_moles() == 30
 
-def test_empirical_vol():
-    q = Quantities('45inch^3',1)
+def test_impirial_vol():
+    q = Quantities('45in3', 1, 'Volume')
 
-    assert q.to_volume == 0.000737418
+    assert q.to_volume() == pytest.approx(0.000737418)
+
+
+def test_prefix_metric_volume():
+
+    assert Quantities('5cm3', 1, 'Volume').to_volume() == pytest.approx(5 * 10**-6)
+    assert Quantities('12mm3', 1, 'Volume').to_volume() == pytest.approx(12 * 10**-9)
+    assert Quantities('25.6cm3', 1, 'Volume').to_volume() == pytest.approx(25.6 * 10**-6)
--- tests/core/test_units.py
+++ tests/core/test_units.py
@@ -6,19 +6,19 @@ from core.units import MassUnit, VolumeUnit
 def test_mass_power():
     test = MassUnit('kg')
 
-    assert test.final_exponent == 1
+    assert test.final_exponent == 3
 
 def test_volume_power_1():
     test = VolumeUnit('ml')
 
-    assert test.final_exponent == 1
+    assert test.final_exponent == -3
 
 def test_volume_power_2():
-    test = VolumeUnit('m^3')
+    test = VolumeUnit('m3')
 
-    assert test.final_exponent == 3
+    assert test.final_exponent == 0
 
 def test_volume_power_3():
-    test = VolumeUnit('cm^3')
+    test = VolumeUnit('cm3')
 
     assert test.final_exponent == -6
